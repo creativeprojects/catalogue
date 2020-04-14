f6dffb8eb639c103d39fc1efbf8f797f0d770af8
Fred <fred@creativeprojects.tech>
2020-04-14 15:12:44 +0100

    Add cstring package

---
+++ cstring/cstring.go
@@ -0,0 +1,18 @@
+package cstring
+
+func Int8ToBytes(value []int8) []byte {
+	length := calculateLen(value)
+	output := make([]byte, length)
+	for index := 0; index < length; index++ {
+		output[index] = byte(value[index])
+	}
+	return output
+}
+
+func calculateLen(value []int8) int {
+	length := 0
+	maxLength := len(value)
+	for length = 0; length < maxLength && value[length] != 0; length++ {
+	}
+	return length
+}
